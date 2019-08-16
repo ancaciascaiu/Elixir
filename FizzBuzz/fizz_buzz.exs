d24a21b9bda1b58b1a5a2fbe729a87e9eb5bc3d8
Anca Ciascaiu <aciascaiu@cars.com>
2019-08-16 16:32:42 -0500

    refactor first solution

--- FizzBuzz/fizz_buzz.exs
+++ FizzBuzz/fizz_buzz.exs
@@ -2,22 +2,24 @@ defmodule FizzBuzz do
   def fizz_buzz do
     1..100
     |> Enum.to_list()
-    |> Enum.map(fn number ->
-      cond do
-        rem(number, 3) == 0 && rem(number, 5) == 0 ->
-          "FizzBuzz"
-
-        rem(number, 3) == 0 ->
-          "Fizz"
-
-        rem(number, 5) == 0 ->
-          "Buzz"
-
-        true ->
-          number
-      end
-    end)
+    |> Enum.map(&convert(&1))
     |> IO.inspect()
+  end
+
+  defp convert(number) do
+    cond do
+      rem(number, 3) == 0 && rem(number, 5) == 0 ->
+        "FizzBuzz"
+
+      rem(number, 3) == 0 ->
+        "Fizz"
+
+      rem(number, 5) == 0 ->
+        "Buzz"
+
+      true ->
+        number
+    end
   end
 end
 
